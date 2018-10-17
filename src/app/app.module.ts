5484141d25fe71e0bda825d0f919e61080d51498
Yilmar Alzate Rodriguez <yilmar.i.am@gmail.com>
2018-10-16 19:09:33 -0500

    Inicio del proyecto. Primero commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HOMEComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HOMEComponent
   ],
   imports: [
     BrowserModule
